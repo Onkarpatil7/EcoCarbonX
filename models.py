7e4a9d4187a3786a532ae0c61da10b8a0980ca98
Onkar Patil <onkarp2005@gmail.com>
2026-06-04 13:20:20 +0530

    Docs added

--- models.py
+++ models.py
@@ -21,27 +21,10 @@ class CompanyProfile(Base):
     __tablename__ = "company_profiles"
 
     id = Column(Integer,primary_key=True,index=True)
-
-    company_name = Column(
-        String,
-        nullable=False
-    )
-
-    industry = Column(
-        String,
-        nullable=False
-    )
-
-    location = Column(
-        String,
-        nullable=False
-    )
-
-    email = Column(
-        String,
-        unique=True,
-        nullable=False
-    )
+    company_name = Column(String,nullable=False)
+    industry = Column(String,nullable=False)
+    location = Column(String,nullable=False)
+    email = Column(String,unique=True,nullable=False)
 
 
 class Marketplace(Base):
@@ -49,9 +32,6 @@ class Marketplace(Base):
     __tablename__ = "marketplace"
 
     id = Column(Integer, primary_key=True, index=True)
-
     company_name = Column(String, nullable=False)
-
     industry = Column(String, nullable=False)
-
     credits = Column(Integer, default=100)
